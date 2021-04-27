1906add30df36a419eb8e6b6ce0347e04da11867
py-cc <paolacarrero731@gmail.com>
2021-04-27 17:14:22 -0500

    fix end parameter

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,10 @@ if number < 0:
     last_digit = last_digit * -1
 elif last_digit > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".format(
-        number, last_digit))
+        number, last_digit, end=' '))
 elif last_digit == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
+    print("Last digit of {:d} is {:d} and is 0".format(
+        number, last_digit, end=' '))
 elif last_digit < 6:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(
-        number, last_digit))
+        number, last_digit, end=' '))
