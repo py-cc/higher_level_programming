92b1ce4d2de13017785cda2d9c925523f11e04ad
py-cc <paolacarrero731@gmail.com>
2021-04-27 17:05:48 -0500

    fix type data include

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ last_digit = abs(number) % 10
 if number < 0:
     last_digit = last_digit * -1
 elif last_digit > 5:
-    print("Last digit of {:d} and is greater than 5".format(
+    print("Last digit of {:d} is {:d} and is greater than 5".format(
         number, last_digit))
 elif last_digit == 0:
-    print("Last digit of {:d} and is 0".format(number, last_digit))
+    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
 elif last_digit < 6:
-    print("Last digit of {:d} and is less than 6 and not 0".format(
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(
         number, last_digit))
