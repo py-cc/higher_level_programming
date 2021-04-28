7523d040c7e998807f5652dd255c9b0ce1b950b9
py-cc <paolacarrero731@gmail.com>
2021-04-27 21:44:48 -0500

    add file to check

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def uppercase(str):
+    x = len(str)
+    count = 0
+    index = 0
+    while count < x:
+        if ord(str[index]) > 96 and ord(str[index]) < 123:
+            print("{:c}".format(ord(str[index]) - 32), end='')
+        else:
+            print(str[index], end='')
+        index = index + 1
+        count = count + 1
+        if count == x - 1:
+            print()
+            break
