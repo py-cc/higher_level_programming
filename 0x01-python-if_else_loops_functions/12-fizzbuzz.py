5602daced9e09a9c62468fffedc0db50ca39ff0f
py-cc <paolacarrero731@gmail.com>
2021-04-27 22:42:32 -0500

    fix conditional

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
-        if (i % 3 == 0) and (i % 5 == 0):
-            print("FizzBuzz ", end='')
-        elif i % 5 == 0:
-            print("Buzz", end='')
-        elif i % 3 == 0:
-            print("Fizz ", end='')
-        else:
-            print("{:d} ".format(i), end='')
+    for a in range(1, 101):
+        if (a % 3 == 0) and (a % 5 == 0):
+            a = "FizzBuzz"
+        elif a % 3 == 0:
+            a = "Fizz"
+        elif a % 5 == 0:
+            a = "Buzz"
+        print('{} '.format(a), end='')
