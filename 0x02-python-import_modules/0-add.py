180ffcb10ac1bea4b5104e605ba7f105ac99eb04
py-cc <paolacarrero731@gmail.com>
2021-04-28 20:03:22 -0500

    add file to check second test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
 
-
-def main():
-
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
     suma = add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, suma))
-
-
-if __name__ == "__main__":
-    main()
