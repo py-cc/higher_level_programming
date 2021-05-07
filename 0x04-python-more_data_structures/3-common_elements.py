393d97eef822b278ccb764a53c56a00a00633e4a
py-cc <paolacarrero731@gmail.com>
2021-05-06 21:27:13 -0500

    fix logic common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if len(set.intersection(set_2)) > 0:
-        string = set_1.intersection(set_2)
-    return string
+    return set_1 & set_2
