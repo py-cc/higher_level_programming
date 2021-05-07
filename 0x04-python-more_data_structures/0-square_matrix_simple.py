f21dcc03159ec16fead6b8aea32811e55a9d5a2c
py-cc <69390957+py-cc@users.noreply.github.com>
2021-05-07 16:52:24 -0500

    Delete 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,6 +0,0 @@
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for item in matrix:
-        list_square = list(map(lambda x: x ** 2, item))
-        new_matrix.append(list_square)
-    return new_matrix
