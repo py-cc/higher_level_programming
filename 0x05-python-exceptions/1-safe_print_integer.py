8d289bd36766a656b9ac8822476f8daa4dcc6094
py-cc <paolacarrero731@gmail.com>
2021-05-17 18:52:17 -0500

    fix style betty and condition return true o false

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value), end='')
-    except ValueError:
-            return False
-    print()
-    return True
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
