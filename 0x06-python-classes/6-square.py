b23cfab97a6e053fb4833a4931d0c6ea96b8169c
py-cc <paolacarrero731@gmail.com>
2021-05-21 18:54:53 -0500

    add file to check

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+""" File with class Square """
+
+
+class Square:
+    """Class Square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ __init__ constructor method"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2
+        raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """ area returns the current square area """
+        return self.size ** 2
+
+    def my_print(self):
+        """ print square """
+        not self.size and print()
+        [print(self.size * "#") for _ in range(self.size)]
