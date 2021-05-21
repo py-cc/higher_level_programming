9544d74b40b4f884167a63bcdab9f3ed1a2ff6a1
py-cc <paolacarrero731@gmail.com>
2021-05-21 15:55:31 -0500

    add file to check

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" File with class Square """
+
+
+class Square:
+    """Class Square"""
+
+    def __init__(self, size):
+        """ __init__ constructor method"""
+        self.__size = size
