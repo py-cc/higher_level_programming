838a801fb786302b6c6b16b3fcad36f64eb00265
py-cc <paolacarrero731@gmail.com>
2021-05-21 16:09:40 -0500

    add file to check

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" File with class Square """
+
+
+class Square:
+    """Class Square"""
+
+    def __init__(self, size=0):
+        """ __init__ constructor method"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
