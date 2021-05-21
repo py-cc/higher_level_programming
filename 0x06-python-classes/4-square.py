f292822be2ab19c2bf7b7e876a83db08315d0eb2
py-cc <paolacarrero731@gmail.com>
2021-05-21 18:09:19 -0500

    add file to check

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,11 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         """ __init__ constructor method"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
