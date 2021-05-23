39bf8fd60270a7f8a381158c3c315f361e79ef02
py-cc <paolacarrero731@gmail.com>
2021-05-22 22:21:45 -0500

    add file to check

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """ Module that define a class rectangle """
-    
+
+
 class Rectangle:
-    
+
     """ Class that defines a rectangle """
-    
+
     def __init__(self, width=0, height=0):
         """ Init method to initialized a instance"""
         self.width = width
         self.height = height
-    
+
     @property
     def height(self):
         """ Method getter for height atributte"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """ Method setter for height atributte """
@@ -24,12 +25,12 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-    
+
     @property
     def width(self):
         """ Method getter for width atributte"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """ Method setter for width atributte """
@@ -39,18 +40,18 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-    
+
     def area(self):
         """ Method that calculates area of a rectangle """
         return self.width * self.height
-    
+
     def perimeter(self):
         """ Method that calculates perimeter of a rectangle """
         if self.width == 0 or self.height == 0:
             return 0
         else:
             return (self.width + self.height) * 2
-    
+
     def __str__(self):
         """ Method that returns a rectangle by # character """
         if self.width == 0 or self.height == 0:
@@ -62,9 +63,10 @@ class Rectangle:
 
     def __repr__(self):
         """ Method that returns a rectangle by # character """
-        repr_t = "Rectangle({},{})".format(self.width, self.height)
+        repr_t = "Rectangle({}, {})".format(
+            self.width, self.height)
         return repr_t
-    
+
     def __del__(self):
         """ Method that deletes a object """
         print("Bye rectangle...")
