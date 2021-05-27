ca05a008486ec56ae3307573ba5761bb13168b2f
py-cc <paolacarrero731@gmail.com>
2021-05-26 22:04:18 -0500

    add file to check

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module function that prints a text with 2 new lines
+    after each of these characters: ., ? and :"""
+
+
+
+def text_indentation(text):
+    """Function that function that prints a text with 2 new lines
+        after each of these characters: ., ? and :.
+    Returns:
+        Nothing.
+    Raises:
+        TypeError: If text is not a str data type
+    """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+    i = 0
+    text = text.strip(' ')
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in (".", "?", ":"):
+            print('\n')
+            if (i + 1) < len(text):
+                while text[i + 1] == " ":
+                    i += 1
+        i += 1
