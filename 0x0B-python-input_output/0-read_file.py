f1caf5443bb7e78d0ab8b3cdc01f2c92522f2abe
py-cc <paolacarrero731@gmail.com>
2021-05-31 21:35:06 -0500

    fix comment

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,14 @@
 """Module that read a file"""
 
 
+
 def read_file(filename=""):
+    """Method that reads a file and prints to stdout
+    Args:
+       filename: name of the file to read.
+    Return:
+       Always nothing.
+    """
     with open(filename, mode='r', encoding='utf-8') as f:
         text = f.read()
         print(text, end='')
