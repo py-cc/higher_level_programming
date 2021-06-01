b10f38bfc9fc85be7e4679008600068dcbfadc95
py-cc <paolacarrero731@gmail.com>
2021-05-31 21:27:48 -0500

    add file to check

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module that read a file"""
+
+
 def read_file(filename=""):
-    with open(filename, mode='r', encoding='utf-8') as file:
-        data = file.read()
-        print(data, end='')
+    with open(filename, mode='r', encoding='utf-8') as f:
+        text = f.read()
+        print(text, end='')
