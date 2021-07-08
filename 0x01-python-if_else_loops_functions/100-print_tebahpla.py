4c53e7206343341d49380701c3b8242d231059fd
py-cc <paolacarrero731@gmail.com>
2021-07-08 09:38:59 -0500

    fix condition only use format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,6 +3,6 @@ a_range = range(97, 123)
 reverser_range = reversed(a_range)
 for i in reverser_range:
     if i % 2 == 0:
-        print(chr(i), end='')
+        print("{:s}".format(chr(i)), end='')
     else:
-        print(chr(i).upper(), end='')
+        print("{:s}".format(chr(i).upper()), end='')
