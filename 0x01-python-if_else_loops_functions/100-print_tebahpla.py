1b56cd71b2c576590bba1bc16da2762ba7c4ed75
py-cc <paolacarrero731@gmail.com>
2021-07-08 09:33:30 -0500

    fix condition variable char

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,8 +2,7 @@
 a_range = range(97, 123)
 reverser_range = reversed(a_range)
 for i in reverser_range:
-    char = chr(i)
     if i % 2 == 0:
-        print(char, end='')
+        print(chr(i), end='')
     else:
-        print(char.upper(), end='')
+        print(chr(i).upper(), end='')
