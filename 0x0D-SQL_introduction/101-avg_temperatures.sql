dbf53dae19040f748117a099f2ed118117f8d021
py-cc <paolacarrero731@gmail.com>
2021-07-13 12:26:21 -0500

    add advances files to check

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,7 @@
+-- Import in hbtn_0c_0 a temperature database.
+-- Displays the average temperature (Fahrenheit) by city
+-- ordered by temperature (descending).
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
