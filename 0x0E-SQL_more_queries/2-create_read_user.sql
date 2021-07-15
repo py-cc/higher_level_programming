9f3901adc2f39bfb20aac76187c1b24678ff54df
py-cc <paolacarrero731@gmail.com>
2021-07-14 23:40:06 -0500

    add file to check

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,6 +5,6 @@
 -- If the user user_0d_2 already exists, the script should not fail.
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
-CREATE DATABASES IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 USE hbtn_0d_2;
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
